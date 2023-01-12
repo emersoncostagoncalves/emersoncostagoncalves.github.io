b5709353e57d8a6b305f7f419c0db1d534ce9cc5
emersoncostagoncalves <emerson.pixxart@gmail.com>
2023-01-12 17:25:01 -0300

    up

--- pokemon-project/src/components/SearchBar.jsx
+++ pokemon-project/src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ import searchIcon from "../icons/search-icon.svg"
 export default function SearchBar({func}) {
     return <div className="search-bar">
         <div className="search-bar-info">
-            <h1>Selecione seu Pokémon</h1>
+            <h1>Selecione o seu Pokémon</h1>
         </div>
         <div className="search-bar-input">
             <input onKeyUp={func} className="search-input" placeholder="Pesquise por nome ou código..." type="text" name="" id="" />
