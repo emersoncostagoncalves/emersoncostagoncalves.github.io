bb9008f928493a55089aecc56412be15503fe780
emersoncostagoncalves <emerson.pixxart@gmail.com>
2023-03-07 15:21:56 -0300

    Update MenuMobileToggle.jsx

--- pokemon-project/src/components/MenuMobileToggle.jsx
+++ pokemon-project/src/components/MenuMobileToggle.jsx
@@ -2,35 +2,28 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
-import "../sass/MenuMobileToggle.scss"
+import "../sass/MenuMobileToggle.scss";
 
 export default function MenuMobileToggle() {
+  const [menuMobile, setmenuMobile] = useState("");
 
-const [menuMobile, setmenuMobile] = useState("")
+  useEffect(() => {
+    setmenuMobile(document.querySelector(".menu-mobile-container"));
+  }, []);
 
+  function displayMenuMobile() {
+    menuMobile.classList.toggle("display");
+    console.log("cliquei no label");
+  }
 
-
-useEffect(() => {
-    setmenuMobile(document.querySelector(".menu-mobile-container"))
-    
-})
-
-function displayMenuMobile(){
- menuMobile.classList.toggle("display")
- console.log("cliquei no label")
-    
-
-}
-
-
-    return (
-        <div className="menu-toggle-container" >
-            <input onChange={displayMenuMobile}id="checkbox" type="checkbox" ></input>
-            <label className="menu-label" htmlFor="checkbox">
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
-        </div>
-    )
+  return (
+    <div className="menu-toggle-container">
+      <input onChange={displayMenuMobile} id="checkbox" type="checkbox"></input>
+      <label className="menu-label" htmlFor="checkbox">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+    </div>
+  );
 }
